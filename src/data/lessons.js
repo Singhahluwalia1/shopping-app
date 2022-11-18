8f5674a6e698ad77010f62e1ff9dc25b1b4be677
Tw <Tw473@live.mdx.ac.uk>
2022-11-18 20:40:11 +0530

    add lessons.js

---
+++ src/data/lessons.js
@@ -0,0 +1,34 @@
+const lessons = [
+  {
+    subject: "Math",
+    location: "London",
+    price: 100,
+    spaces: 5
+  },
+  {
+    subject: "Math",
+    location: "Oxford",
+    price: 100,
+    spaces: 5
+  },
+  {
+    subject: "English",
+    location: "London",
+    price: 100,
+    spaces: 5
+  },
+  {
+    subject: "English",
+    location: "York",
+    price: 80,
+    spaces: 5
+  },
+  {
+    subject: "Music",
+    location: "Bristol",
+    price: 90,
+    spaces: 5
+  }
+];
+
+export default lessons;
