15a34e8d39c08c0309f9a5e34e0c0df418c1fbb6
Tw <Tw473@live.mdx.ac.uk>
2022-11-18 20:45:44 +0530

    Add sort menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 <template>
 
     <div class="row m-2">
+        <div class="col-sm-2">
+            <div class="row">
+                <h5>Sort by</h5>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="sort-by" v-model="sortby" value="subject" />
+                    Subject
+                </label>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="sort-by" v-model="sortby" value="location" />
+                    Location
+                </label>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="sort-by" v-model="sortby" value="price" />
+                    Price
+                </label>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="sort-by" v-model="sortby" value="spaces" />
+                    Availability
+                </label>
+            </div>
+            <div class="row mt-5">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="order" v-model="orderby" value="asc" />
+                    Ascending
+                </label>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="order" v-model="orderby" value="desc" />
+                    Descending
+                </label>
+            </div>
+        </div>
         <div class="col">
          </div>
     </div>
