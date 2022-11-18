47d51866dc9a11a947a0d2936d738171525d33c1
Tw <Tw473@live.mdx.ac.uk>
2022-11-18 20:42:35 +0530

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="row m-2">
+        <div class="col">
+         </div>
+    </div>
+
+  
+</template>
+
+<script>
+
+import lessons from "./data/lessons";
+import "./assets/fontawesome.css";
+
+export default {
+    name: "App",
+    
+    
+};
+
+</script>
+
+<style>
+
+</style>
